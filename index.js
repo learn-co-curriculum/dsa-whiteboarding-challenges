a83ef05dd175c6512980cbf06a64566ae2a2ae11
Maxwell Benton <maxwellbenton@gmail.com>
2020-11-16 12:13:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ fetch('https://raw.githubusercontent.com/learn-co-curriculum/dsa-whiteboarding-c
 .then(res => res.json())
 .then(obj => {
   let main = document.querySelector('main')
+  for(let n = 0; n < obj.length; n++) {
+    let li = document.createElement('li')
+    li.textContent = obj[n]
+    main.appendChild(li)
+  }
 console.log(obj)
 })
