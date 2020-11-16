f27ade46fe2a5e2ee20d433b651cedf6d3e83de4
Maxwell Benton <maxwellbenton@gmail.com>
2020-11-16 12:12:09 -0500

    fix ttypo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 fetch('https://raw.githubusercontent.com/learn-co-curriculum/dsa-whiteboarding-challenges/main/completionists.json')
-.then(res => res.json()
+.then(res => res.json())
 .then(obj => {
   let main = document.querySelector('main')
 console.log(obj)
